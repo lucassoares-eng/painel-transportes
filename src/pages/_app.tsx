5ef03ad5857fffa5b72facab1b2d7ae7a897263a
lucas.soares <lucas_soares.eng@hotmail.com>
2021-07-28 23:22:41 -0300

    Adiciona colors e font-family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   body {
+    font-family: 'Nunito', sans-serif;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
@@ -11,7 +12,31 @@ const GlobalStyle = createGlobalStyle`
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    background: '#343A40',
+    sidebar: '#3A444E',
+    card_primary: '#37404A',
+    card_secondary: '#444e5a',
+    text_primary: '#FFFFFF',
+    text_secondary: '#8391A2',
+    text_focus: '#AAB8C5',
+    text_link: '#CED4DA',
+    blue: '#2c8ef8',
+    indigo: '#727cf5',
+    purple: '#6b5eae',
+    pink: '#ff679b',
+    red: '#fa5c7c',
+    orange: '#fd7e14',
+    yellow: '#ffbc00',
+    green: '#0acf97',
+    teal: '#02a8b5',
+    cyan: '#39afd1',
+    white: '#fff',
+    gray: '#8391a2',
+    gray_dark: '#e3eaef',
+    success: '#0acf97',
+    info: '#39afd1',
+    warning: '#ffbc00',
+    danger: '#fa5c7c',
   },
 }
 
