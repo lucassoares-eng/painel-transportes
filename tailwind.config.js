a022efcda130f8dd22236823af8df7e4c38ded27
lucas.soares <lucas_soares.eng@hotmail.com>
2021-08-02 18:14:17 -0300

    Adiciona extend height

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: theme => ({
+        "screen-80": "80vh",
+        "screen-90": "90vh",
+      }),
+    },
   },
   variants: {
     extend: {},
